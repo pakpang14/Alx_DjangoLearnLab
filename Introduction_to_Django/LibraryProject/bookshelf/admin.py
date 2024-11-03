6aa10bb03d7b294908fea7c2e73aed4cd3e8ffef
pakpang14 <ibrahim.ammed90@gmail.com>
2024-11-03 14:15:52 +0000

    admin-superuser created successfully

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Book
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'autor')
+
+admin.site.register(Book, BookAdmin)
